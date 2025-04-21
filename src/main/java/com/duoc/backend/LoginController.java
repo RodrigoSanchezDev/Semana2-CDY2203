a1adf5a4b0aeeeff251a4652924c08844839a560
RodrigoSanchez35 <rodr.sanchezc@duocuc.cl>
2025-04-20 23:47:25 -0600

    Commit

---
+++ src/main/java/com/duoc/backend/LoginController.java
@@ -0,0 +1,43 @@
+package com.duoc.backend;
+import com.duoc.backend.JWTAuthenticationConfig;
+import com.duoc.backend.user.MyUserDetailsService;
+import com.duoc.backend.user.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    JWTAuthenticationConfig jwtAuthtenticationConfig;
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/login")
+    public String login(@RequestBody User loginRequest) {
+
+        /**
+        * En el ejemplo no se realiza la correcta validación del usuario
+        */
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequest.getUsername());
+
+        if (!passwordEncoder.matches(loginRequest.getPassword(), userDetails.getPassword())) {
+            throw new RuntimeException("Invalid login");
+        }
+
+        String token = jwtAuthtenticationConfig.getJWTToken(loginRequest.getUsername());
+        return token;
+    }
+
+}
---
+++ src/main/java/com/duoc/backend/user/UserController.java
@@ -0,0 +1,25 @@
+package com.duoc.backend.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody User user) {
+        // Hash the password before saving
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return "User registered successfully!";
+    }
+}
